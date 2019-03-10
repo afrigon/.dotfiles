13e30cdb8326b44c6acfd2aa2bc5b664cd012fd6
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-03-10 19:07:03 -0400

    fixed dock defaults

--- macos.sh
+++ macos.sh
@@ -35,20 +35,21 @@ sudo systemsetup -settimezone "America/Montreal" > /dev/null
 dockutil --no-restart --remove all
 dockutil --no-restart --add /Applications/Launchpad.app
 dockutil --no-restart --add /Applications/Messages.app
-dockutil --no-restart --add /Applications/Safari.app
+dockutil --no-restart --add /Applications/Utilities/Terminal.app
 dockutil --no-restart --add /Applications/Google\ Chrome.app
 dockutil --no-restart --add /Applications/iTunes.app
 dockutil --no-restart --add /Applications/Slack.app
 dockutil --no-restart --add /Applications/Visual\ Studio\ Code\ -\ Insiders.app
 dockutil --no-restart --add /Applications/Xcode.app
 dockutil --no-restart --add /Applications/Calendar.app
-dockutil --no-restart --add /Applications/Reminders.app
-dockutil --no-restart --add /Applications/Utilities/Terminal.app
 dockutil --no-restart --add /Applications/System\ Preferences.app
 dockutil --no-restart --add "$HOME/Downloads" --view fan --display stack --sort dateadded
 
-# Set the icon size of Dock items to 44 pixels
-defaults write com.apple.dock tilesize -int 44
+# Set the icon size of Dock items to 62 pixels
+defaults write com.apple.dock tilesize -int 62
+
+# Auto hides the dock
+defaults write com.apple.dock autohide --int 1
 
 # Disable audio feedback when volume is changed
 defaults write com.apple.sound.beep.feedback -bool false
